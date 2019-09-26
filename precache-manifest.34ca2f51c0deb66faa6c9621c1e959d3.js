4a001cbbd4e6d0a2bb883b1a15047e0d552e52d2
ridwanmaulana8817 <ridwanmaulanacirebon@gmail.com>
2019-09-26 14:04:38 +0700

    Updates

---
+++ precache-manifest.34ca2f51c0deb66faa6c9621c1e959d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335dbc47434b768f79cef8226247001f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "3726b98c055efc043239",
+    "url": "/resume/static/css/2.1d7cc79d.chunk.css"
+  },
+  {
+    "revision": "1a0ad9d56a72f1669d7b",
+    "url": "/resume/static/css/main.b6832cad.chunk.css"
+  },
+  {
+    "revision": "3726b98c055efc043239",
+    "url": "/resume/static/js/2.6bf47fbf.chunk.js"
+  },
+  {
+    "revision": "1a0ad9d56a72f1669d7b",
+    "url": "/resume/static/js/main.7a24672d.chunk.js"
+  },
+  {
+    "revision": "e749c93fa0dc9adc6c07",
+    "url": "/resume/static/js/runtime~main.52cbd131.js"
+  }
+]);
